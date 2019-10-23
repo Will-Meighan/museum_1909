4f0fd25a7a29ddf826833dc71eba82cd41e01ebc
William Meighan <williamdmeighan@gmail.com>
2019-10-23 09:13:47 -0600

    Complete iteration 1

---
+++ test/exhibit_test.rb
@@ -0,0 +1,22 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/exhibit'
+
+class ExhibitTest < Minitest::Test
+
+  def setup
+    @exhibit = Exhibit.new("Gems and Minerals", 0)
+  end
+
+  def test_it_exists
+    assert_instance_of Exhibit, @exhibit
+  end
+
+  def test_it_has_attributes
+    assert_equal "Gems and Minerals", @exhibit.name
+    assert_equal 0, @exhibit.cost
+  end
+
+
+end
