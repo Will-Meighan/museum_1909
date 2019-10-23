0a482c127f5679b7ecef56bea32c45457d5d21bd
William Meighan <williamdmeighan@gmail.com>
2019-10-23 10:55:53 -0600

    Add unfinished patrons by exhibit interest method and test

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,11 @@
 class Museum
 
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -15,6 +16,29 @@ class Museum
     @exhibits.find_all do |exhibit|
       patron.interests.include?(exhibit.name)
     end
+  end
+
+  def admit(patron)
+    @patrons << patron
+  end
+
+
+
+  def patrons_by_exhibit_interest
+    # We need to return a hash with the exhibit instance as the key, and an array of each interested patron instance as the value
+    hash = Hash.new
+
+require 'pry'
+binding.pry
+
+    @exhibits.map do |exhibit|
+      hash[exhibit] = []
+      @patrons.map do |patron|
+       # need an if statement or a helper method of some sort
+
+      end
+    end
+    hash
   end
 
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -21,6 +21,7 @@ class MusuemTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "Denver Museum of Nature and Science", @dmns.name
     assert_equal [], @dmns.exhibits
+    assert_equal [], @dmns.patrons
   end
 
   def test_it_can_add_exhibit
@@ -42,6 +43,34 @@ class MusuemTest < Minitest::Test
 
     assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@bob)
     assert_equal [@imax], @dmns.recommend_exhibits(@sally)
+  end
+
+  def test_it_can_admit_patrons
+    @dmns.admit(@bob)
+    @dmns.admit(@sally)
+
+    assert_equal [@bob, @sally], @dmns.patrons
+  end
+
+  def test_it_can_calculate_patrons_by_exhibit_interest
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    @bob.add_interest("Dead Sea Scrolls")
+    @bob.add_interest("Gems and Minerals")
+    @sally.add_interest("Dead Sea Scrolls")
+
+    @dmns.admit(@bob)
+    @dmns.admit(@sally)
+
+    hash = {
+            @gems_and_minerals => [@bob],
+            @dead_sea_scrolls => [@bob, @sally],
+            @imax => []
+            }
+
+    assert_equal hash, @dmns.patrons_by_exhibit_interest
   end
 
 end
